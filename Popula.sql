0c2cb562cad2375760e489891ef9d84aec0969e9
marcossc <ms.castanheira@gmail.com>
2016-04-05 09:56:59 -0300

    Added files via upload

---
+++ Popula.sql
@@ -0,0 +1,59 @@
+-- Aluno
+
+INSERT INTO Aluno (nome, matricula, email, endereco, telefone, cidade, estado, bairro) VALUES
+('Marcos',123456,'ms.castanheira@gmail.com','Rua Jackson de Figueiredo, 295','5198450033','Porto Alegre', 'RS', 'Sarandi');
+
+INSERT INTO Aluno (nome, matricula, email, endereco, telefone, cidade, estado, bairro) VALUES
+('Thiago',234567,'thiago@gmail.com','Rua dos Andradas, 295','5182901771','Porto Alegre', 'RS', 'Centro');
+
+INSERT INTO Aluno (nome, matricula, email, endereco, telefone, cidade, estado, bairro) VALUES
+('Cristiano',345678,'cristiano@gmail.com','Av Cavalhada, 295','5192257575','Porto Alegre', 'RS', 'Cavalhada');
+
+INSERT INTO Aluno (nome, matricula, email, endereco, telefone, cidade, estado, bairro) VALUES
+('Luis Henrique',456789,'lh@gmail.com','Rua 7 de Setembro, 295','5191633250','Porto Alegre', 'RS', 'Centro');
+
+INSERT INTO Aluno (nome, matricula, email, endereco, telefone, cidade, estado, bairro) VALUES
+('Vanessa',567890,'vanessa@gmail.com','Rua Cel Genuíno, 295','5195559118','Porto Alegre', 'RS', 'Centro');
+
+
+-- Área de interesse
+
+INSERT INTO AreaInteresse (nomeArea) VALUES
+('Desenvolvimento Desktop'),
+('Desenvolvimento Web'),
+('Banco de Dados'),
+('Gerência de Projetos'),
+('Teste de Software');
+
+
+-- Professor
+
+INSERT INTO Professor (nome, matricula, email, endereco, telefone, cidade, estado, bairro, numVagas, turnoDia, turnoNoite) VALUES
+('Rafael',123456,'rafael@senacrs.com.br','Rua Cel Genuíno, 130','5130221044','Porto Alegre', 'RS', 'Centro', 5, 1, 1);
+
+INSERT INTO Professor (nome, matricula, email, endereco, telefone, cidade, estado, bairro, numVagas, turnoDia, turnoNoite) VALUES
+('Aline',234567,'aline@senacrs.com.br','Rua Cel Genuíno, 130','5130221044','Porto Alegre', 'RS', 'Centro', 3, 0, 1);
+
+INSERT INTO Professor (nome, matricula, email, endereco, telefone, cidade, estado, bairro, numVagas, turnoDia, turnoNoite) VALUES
+('Ries',345678,'ries@senacrs.com.br','Rua Cel Genuíno, 130','5130221044','Porto Alegre', 'RS', 'Centro', 4, 1, 0);
+
+INSERT INTO Professor (nome, matricula, email, endereco, telefone, cidade, estado, bairro, numVagas, turnoDia, turnoNoite) VALUES
+('Carbonera',456789,'carbonera@senacrs.com.br','Rua Cel Genuíno, 130','5130221044','Porto Alegre', 'RS', 'Centro', 6, 0, 1);
+
+INSERT INTO Professor (nome, matricula, email, endereco, telefone, cidade, estado, bairro, numVagas, turnoDia, turnoNoite) VALUES
+('Lizandro',567890,'rafael@senacrs.com.br','Rua Cel Genuíno, 130','5130221044','Porto Alegre', 'RS', 'Centro', 2, 1, 1);
+
+
+-- Relaciona Professor com Área de Interesse
+
+INSERT INTO ProfessorAreaInteresse (idAreaInteresse,idProfessor) VALUES
+(1,5),
+(2,4),
+(3,3),
+(4,3),
+(5,1),
+(1,3),
+(2,2),
+(3,4),
+(4,2),
+(5,4);
