807047121fada616cc287a7d000c839fd0a24abc
Thiago Duarte <thiago-duarte@outlook.com>
2016-04-20 23:56:11 +0000

    Correção de bug

--- application/views/includes/prototipo_header.php
+++ application/views/includes/prototipo_header.php
@@ -8,15 +8,3 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
-<<<<<<< HEAD:application/views/includes/prototipo_header.php
-=======
-                    <h1 class="page-header">Blank</h1>
-                    <?php $this->load->view('view_teste')?>
-                <div><!-- /.col-lg-12 -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div><!-- /#page-wrapper -->
-</div><!-- /#wrapper -->
-<!-- Carregar o footer -->
-<?php $this->load->view('includes/footer')?>
->>>>>>> dfd703927c7d3e3c52e1743ea5ca5185b5e1fc7e:application/views/prototipo.php
