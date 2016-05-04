46aa801c9513da2152a283767f479b2ae25a6099
Thiago Duarte <thiago.duarte@outlook.com>
2016-05-04 02:08:00 -0300

    Alteração da interface Main e alteração do menu lateral

--- application/views/home/main.php
+++ application/views/home/main.php
@@ -1,7 +1,21 @@
+<style type="text/css">
+    .main {
+        max-width: 500px;
+        margin: 20px auto;
+    }
+</style>
+<div class="main">
+    <br>
+    <br>
+    <br>
 <h3>MyTCC - Integrando orientandos e orientadores.</h3>
 <hr>
 <p>Bem-vindo ao <strong>MyTCC</strong>. Aplicação criada por estudantes do SENACRS em 2016/1.</p> </br>
 
 <div ng-controller="alunoController">
-    <input class="btn btn-primary btn-lg" type="button" value="Registre-se agora!" ng-click="open()"/> </br> <br>
+    <input class="btn btn-primary btn-lg" type="button" value="Registre-se agora!" ng-click="open()"/>
+    <a class="btn btn-primary" href="./login" role="button">Entrar</a>
 </div>
+</div>
+
+
