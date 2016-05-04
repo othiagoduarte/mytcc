d774d10df2d408127fbd6c647ea0a91edd8b9b7d
luishenriquepp <peppyno@gmail.com>
2016-05-04 15:06:01 -0300

    Inclusão de dados na tela main e do home controller
    O Home controller agora é responsável por todos os dados das telas de
    registro

--- assets/angularjs/controllers/homeController.js
+++ assets/angularjs/controllers/homeController.js
@@ -1,21 +1,64 @@
 angular.module('mytcc')
 
-.controller('homeController', function($scope, $uibModal, $log)
+.controller('homeController', function($scope, $uibModal, $log, $timeout)
 {    
+    $log.log('acessando o home controller..');
     
+    // inicializa os dados que serao enviados pelos formularios no final do processo
+    $scope.data = 
+    { 
+        usuario: 
+        { 
+            cpf: '',
+            senha: '',
+            conf: '',
+            tipo: ''
+        },
+        aluno:
+        {
+            nome: '',
+            email: '',
+            matricula: '',
+            cidade: '',
+            endereco: 'endereco qualquer',
+            bairro: '',
+            estado: '',
+            telefone: ''
+        },
+        professor:
+        {
+            nome: '',
+            email: '',
+            matricula: '',
+            endereco: 'endereco qualquer',
+            cidade: '',
+            bairro: '',
+            estado: '',
+            telefone: '',
+            turnoDia: false,
+            turnoNoite: false,
+            vagas: 1
+        }
+    }
+    
+    $scope.feedback = false;
+    $scope.data.sucesso = false;
+    $scope.data.mensagem = '';
 
+    // abre a modal de 'registre-se'
     $scope.open = function () 
     {
+        $log.log('abrindo modal de cadastro');
         var modalInstance = $uibModal.open
         ({
             animation: true,
-            templateUrl: 'alunos/registrar',
-            controller: 'alunoController',
+            templateUrl: 'alunos/registrarUsuario',
+            controller: 'mUsuarioController',
             resolve: 
             {
                 items: function () 
                 {
-                    return $scope.items;
+                    return $scope.data;
                 }
             }
         });
@@ -23,11 +66,20 @@ angular.module('mytcc')
         modalInstance.result
         .then(function (selectedItem) 
         {
-            $scope.selected = selectedItem;
+            $log.log('result chamado');
+            if($scope.data.sucesso == true)
+            {
+                 setTimeout(function(){location.href="orientacao/solicitar"} , 3000); 
+            }
         }, 
         function () 
         {
             $log.info('Modal dismissed at: ' + new Date());
         });
     };
+    
+    $scope.setFeedBack = function()
+    {
+        $scope.feedback = !$scope.feedback;
+    }
 });
