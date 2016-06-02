0170f0f9448d7df50c974aaaff78ca0186436750
cristiano-lauffer <cristiano.serafim.lauffer@gmail.com>
2016-06-01 21:20:02 -0300

    - Melhoria no script do banco de dados.

--- scripts_banco/mytcc.sql
+++ scripts_banco/mytcc.sql
@@ -1,3 +1,5 @@
+USE mytcc;
+
 -- phpMyAdmin SQL Dump
 -- version 4.5.1
 -- http://www.phpmyadmin.net
@@ -7,14 +9,7 @@
 -- Versão do servidor: 10.1.10-MariaDB
 -- Versão do PHP: 5.6.19
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Banco de dados: `mytcc`
@@ -350,6 +345,4 @@ ALTER TABLE `projeto`
   ADD CONSTRAINT `FK_Projeto_1` FOREIGN KEY (`idProfessor`) REFERENCES `professor` (`id`),
   ADD CONSTRAINT `FK_Projeto_2` FOREIGN KEY (`idAreaInteresse`) REFERENCES `areainteresse` (`id`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
